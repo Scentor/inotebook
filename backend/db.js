72c8d780e273c89118b61406c80871a033cf11ce
scentor <scentoriclegend@gmail.com>
2023-02-21 16:43:30 +0500

    basic

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const mongoUrl = "mongodb://localhost:27017/?directConnection=true&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+mongoose.set('strictQuery', false);
+const connectToMongo = ()=>{
+    mongoose.connect(mongoUrl,()=>{
+        console.log("Connected Successfully")
+    })
+}
+module.exports = connectToMongo
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const db = require("./db.js")
+db()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(' Hassan ! ')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port  http://localhost:${port}`)
+})
